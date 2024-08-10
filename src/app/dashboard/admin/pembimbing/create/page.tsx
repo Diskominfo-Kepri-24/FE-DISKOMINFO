c23c6100c8d7e05dd317415d6aeff411a8a96bdf
muhamadnursyami <nursami385@gmail.com>
2024-08-11 01:03:37 +0700

    feat: registrasi pembimbing dashboard admin

---
+++ src/app/dashboard/admin/pembimbing/create/page.tsx
@@ -0,0 +1,124 @@
+"use client";
+import React, { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import axios from 'axios';
+import DefaultLayout from '@/components/Layouts/DefaultLaout';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useSession } from 'next-auth/react';
+
+const CreatePembimbing = () => {
+  const [nama, setNama] = useState('');
+  const [email, setEmail] = useState('');
+  const [noHp, setNoHp] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    if (!session) {
+      toast.error('Anda harus login terlebih dahulu.');
+      setIsLoading(false);
+      return;
+    }
+
+    const formData = {
+      nama,
+      email,
+      no_hp: noHp,
+      password,
+    };
+
+    try {
+      await axios.post('http://127.0.0.1:8000/api/v1/register/pembimbing', formData);
+      toast.success('Registrasi Pembimbing berhasil ditambahkan!');
+
+      setTimeout(() => {
+        router.push('/dashboard/admin/pembimbing');
+      }, 3000);
+
+    } catch (error) {
+      console.error('Error submitting form:', error);
+      toast.error('Gagal menambahkan pembimbing. Silakan coba lagi.');
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <DefaultLayout>
+      <ToastContainer />
+      <div className="rounded-[10px] border border-stroke bg-white shadow-1 dark:border-dark-3 dark:bg-gray-dark dark:shadow-card">
+        <div className="border-b border-stroke px-6.5 py-4 dark:border-dark-3">
+          <h3 className="font-medium text-dark dark:text-white">
+            Create Pembimbing
+          </h3>
+        </div>
+        <form onSubmit={handleSubmit} className="flex flex-col gap-5.5 p-6.5">
+          <div>
+            <label className="mb-3 block text-body-sm font-medium text-dark dark:text-white">
+              Nama
+            </label>
+            <input
+              type="text"
+              value={nama}
+              onChange={(e) => setNama(e.target.value)}
+              placeholder="Nama"
+              className="w-full rounded-[7px] border-[1.5px] border-stroke bg-transparent px-5.5 py-3 text-dark outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-gray-2 dark:border-dark-3 dark:bg-dark-2 dark:text-white dark:focus:border-primary"
+            />
+          </div>
+          <div>
+            <label className="mb-3 block text-body-sm font-medium text-dark dark:text-white">
+              Email
+            </label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Email"
+              className="w-full rounded-[7px] border-[1.5px] border-stroke bg-transparent px-5.5 py-3 text-dark outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-gray-2 dark:border-dark-3 dark:bg-dark-2 dark:text-white dark:focus:border-primary"
+            />
+          </div>
+          <div>
+            <label className="mb-3 block text-body-sm font-medium text-dark dark:text-white">
+              NoHp
+            </label>
+            <input
+              type="text"
+              value={noHp}
+              onChange={(e) => setNoHp(e.target.value)}
+              placeholder="No HP"
+              className="w-full rounded-[7px] border-[1.5px] border-stroke bg-transparent px-5.5 py-3 text-dark outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-gray-2 dark:border-dark-3 dark:bg-dark-2 dark:text-white dark:focus:border-primary"
+            />
+          </div>
+          <div>
+            <label className="mb-3 block text-body-sm font-medium text-dark dark:text-white">
+              Password
+            </label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password"
+              className="w-full rounded-[7px] border-[1.5px] border-stroke bg-transparent px-5.5 py-3 text-dark outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-gray-2 dark:border-dark-3 dark:bg-dark-2 dark:text-white dark:focus:border-primary"
+            />
+          </div>
+         
+          <button 
+            type="submit" 
+            className="mt-4 bg-blue-500 text-white px-4 py-2 rounded"
+            disabled={isLoading}
+          >
+            {isLoading ? 'Loading...' : 'Create'}
+          </button>
+        </form>
+      </div>
+    </DefaultLayout>
+  );
+};
+
+export default CreatePembimbing;
---
+++ src/components/Tables/TablePembimbing.tsx
@@ -0,0 +1,187 @@
+"use client";
+import axios from "axios";
+import { useSession } from "next-auth/react";
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+interface Pembimbing {
+  id: string;
+  nama: string;
+  email: string;
+  no_hp: string;
+}
+
+const TablePembimbing = () => {
+  const [dataPembimbing, setDataPembimbing] = useState<Pembimbing[]>([]);
+  const [loading, setLoading] = useState<string | null>(null);
+  const router = useRouter();
+  const { data: session } = useSession();
+  // console.log(dataPembimbing);
+  useEffect(() => {
+    if (session && session.accessToken) {
+      const fetchDataPembimbing = async () => {
+        try {
+          const response = await axios.get('http://127.0.0.1:8000/api/v1/pembimbing', {
+            headers: {
+              Authorization: `Bearer ${session.accessToken}`,
+            },
+          });
+        
+          setDataPembimbing(response.data.data);
+        } catch (error) {
+          console.error('Error fetching pembimbing list', error);
+        }
+      };
+      fetchDataPembimbing();
+  }
+  }, [session]);
+
+  const handleCreate = () => {
+    router.push("/dashboard/admin/pembimbing/create");
+  };
+
+  // const handleUpdate = (id: string) => {
+  //   router.push(`/dashboard/admin/pembimbing/update/${id}`);
+  // };
+
+  const handleDelete = async (id: string) => {
+    if (session?.accessToken) {
+      setLoading(id); 
+      try {
+        await axios.delete(`http://127.0.0.1:8000/api/v1/pembimbing/${id}`, {
+          headers: {
+            Authorization: `Bearer ${session.accessToken}`,
+            'Content-Type': 'multipart/form-data'
+          }
+        });
+        toast.success('Pembimbing berhasil dihapus!', {
+          position: "top-right",
+          autoClose: 2000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+        setTimeout(() => {
+          window.location.reload();
+        }, 3000);
+      } catch (error) {
+        toast.error('Gagal menghapus Pembimbing!', {
+          position: "top-right",
+          autoClose: 2000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+      } finally {
+        setLoading(null); // Reset loading state
+      }
+    } else {
+      toast.error('Session tidak ditemukan!', {
+        position: "top-right",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    }
+  };
+
+  return (
+    <div>
+      <ToastContainer />
+      <button
+        onClick={handleCreate}
+        className="mb-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+      >
+        Create Pembimbing
+      </button>
+      <div className="rounded-[10px] border mt-4 border-stroke bg-white p-4 shadow-1 dark:border-dark-3 dark:bg-gray-dark dark:shadow-card sm:p-7.5">
+        <div className="max-w-full overflow-x-auto">
+          <table className="w-full table-auto">
+            <thead>
+              <tr className="bg-[#F7F9FC] text-left dark:bg-dark-2">
+                <th className=" px-4 py-4 font-medium text-dark dark:text-white xl:pl-7.5">
+                  No
+                </th>
+                <th className=" px-4 py-4 font-medium text-dark dark:text-white xl:pl-7.5">
+                  Nama
+                </th>
+                <th className="px-4 py-4 text-center font-medium text-dark dark:text-white xl:pr-7.5">
+                  Email
+                </th>
+                <th className="px-4 py-4 text-center font-medium text-dark dark:text-white xl:pr-7.5">
+                  noHp
+                </th>
+                <th className="px-4 py-4 text-center font-medium text-dark dark:text-white xl:pr-7.5">
+                  Aksi
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {dataPembimbing.map((item, index) => (
+                <tr key={item.id}>
+                  <td
+                    className={`border-[#eee] px-4 py-4 dark:border-dark-3 xl:pl-7.5 ${
+                      index === dataPembimbing.length - 1 ? "border-b-0" : "border-b"
+                    }`}
+                  >
+                    {index + 1}
+                  </td>
+                  <td
+                    className={`border-[#eee] px-4 py-4 dark:border-dark-3 xl:pl-7.5 ${
+                      index === dataPembimbing.length - 1 ? "border-b-0" : "border-b"
+                    }`}
+                  >
+                    <h5 className="text-dark dark:text-white">{item.nama}</h5>
+                  </td>
+                  <td
+                    className={`border-[#eee] px-4 py-4 dark:border-dark-3 xl:pl-7.5 ${
+                      index === dataPembimbing.length - 1 ? "border-b-0" : "border-b"
+                    }`}
+                  >
+                    <h5 className="text-dark dark:text-white">{item.email}</h5>
+                  </td>
+                  <td
+                    className={`border-[#eee] px-4 py-4 dark:border-dark-3 xl:pl-7.5 ${
+                      index === dataPembimbing.length - 1 ? "border-b-0" : "border-b"
+                    }`}
+                  >
+                    <h5 className="text-dark dark:text-white">{item.no_hp}</h5>
+                  </td>
+
+                  <td
+                    className={`border-[#eee] px-4 py-4 dark:border-dark-3 xl:pr-7.5 ${
+                      index === dataPembimbing.length - 1 ? "border-b-0" : "border-b"
+                    }`}
+                  >
+                    <div className="flex items-center justify-end space-x-3.5"> 
+                      <button
+                        onClick={() => handleDelete(item.id)}
+                        className={`bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600 ${
+                          loading === item.id ? "cursor-not-allowed" : ""
+                        }`}
+                        disabled={loading === item.id}
+                      >
+                        {loading === item.id ? "Loading..." : "Hapus"}
+                      </button>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TablePembimbing;
