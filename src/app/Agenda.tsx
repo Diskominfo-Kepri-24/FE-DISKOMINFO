3e2ba28354fa11fc6909e3f3ac62cf88fbf5e52c
muhamadnursyami <nursami385@gmail.com>
2024-08-13 10:42:40 +0700

    feat: getAgenda Portal Web and Filter by Year dan Months

--- src/app/Agenda.tsx
+++ src/app/Agenda.tsx
@@ -1,18 +1,66 @@
 "use client"
-import { useState } from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+export type DataAgenda = {
+  id: string;
+  judul: string;
+  slug: string;
+  status: string;
+  tanggal_mulai: string;
+  tanggal_selesai: string;
+  tipe_acara: string;
+  isi_agenda: string;
+  foto: string;
+  tgl_event_mulai: string;
+  tgl_event_akhir: string;
+  tanggal_event_mulai: string;
+  tanggal_event_akhir: string;
+};
 
 const Agenda = () => {
-  const [selectedYear, setSelectedYear] = useState(2024);
-  const [selectedMonth, setSelectedMonth] = useState('Juni');
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+  const currentMonth = currentDate.toLocaleString('id-ID', { month: 'long' });
+
+  const [agendaData, setAgendaData] = useState<DataAgenda[]>([]);
+  const [selectedYear, setSelectedYear] = useState(currentYear);
+  const [selectedMonth, setSelectedMonth] = useState(currentMonth);
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const fetchAgendaData = async () => {
+    try {
+      const response = await axios.get('http://127.0.0.1:8000/api/v1/agenda');
+      setAgendaData(response.data.agenda);
+    } catch (err) {
+      setError('Failed to fetch data');
+      console.error('Error fetching data:', err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchAgendaData();
+  }, []);
 
   const months = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'];
 
-  const events = [
-    { date: '25 June', time: '09:00 - 12:00', title: 'Seleksi Pekan Kreativesia Pemuda Indonesia Tahun 2024', status: 'Telah Selesai' },
-    { date: '06 June', time: '09:00 - 14:00', title: 'Pemilihan Pemuda Pelopor Tingkat Nasional Tahun 2024', status: 'Telah Selesai' },
-  ];
+  const monthToNumber = (monthName: string) => {
+    return months.indexOf(monthName) + 1;
+  };
 
-  const filteredEvents = events.filter(event => event.date.includes('June')); 
+  const filteredAgendaData = agendaData.filter(agenda => {
+    const eventStartDate = new Date(agenda.tgl_event_mulai);
+    const eventEndDate = new Date(agenda.tgl_event_akhir);
+    const selectedMonthNumber = monthToNumber(selectedMonth);
+
+    return (
+      (eventStartDate.getFullYear() === selectedYear && eventStartDate.getMonth() + 1 === selectedMonthNumber) ||
+      (eventEndDate.getFullYear() === selectedYear && eventEndDate.getMonth() + 1 === selectedMonthNumber)
+    );
+  });
 
   return (
     <div className="p-6 rounded-lg shadow-md bg-white">
@@ -23,7 +71,7 @@ const Agenda = () => {
           value={selectedYear}
           onChange={(e) => setSelectedYear(Number(e.target.value))}
         >
-          {[2024, 2023, 2022].map((year) => (
+          {[2026, 2025, 2024, 2023].map((year) => (
             <option key={year} value={year}>{year}</option>
           ))}
         </select>
@@ -40,20 +88,21 @@ const Agenda = () => {
         ))}
       </div>
       <div>
-        {filteredEvents.length > 0 ? (
-          filteredEvents.map((event, index) => (
-            <div key={index} className="flex items-center mb-6">
+        {filteredAgendaData.length > 0 ? (
+          filteredAgendaData.map((agenda) => (
+            <div key={agenda.id} className="flex items-center mb-6">
               <div className="w-8 h-8 bg-blue-500 rounded-full flex items-center justify-center">
                 <svg className="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 9V7a4 4 0 10-8 0v2m5 4h-4m6 4H6a2 2 0 01-2-2V7a2 2 0 012-2h12a2 2 0 012 2v10a2 2 0 01-2 2z" />
                 </svg>
               </div>
               <div className="ml-4">
-                <a href="/agenda/1">
-                <h3 className="text-sm font-bold hover:text-blue-500 text-slate-700">{event.title}</h3>
-
+                <a href={`/agenda/${agenda.slug}`}>
+                  <h3 className="text-sm font-bold hover:text-blue-500 text-slate-700">{agenda.judul}</h3>
                 </a>
-                <p className="text-sm text-gray-400 mt-1">{event.date}, {event.time} <span className="ml-2 bg-gray-100 text-xs text-gray-500 font-semibold py-1 px-2 rounded-lg">{event.status}</span></p>
+                <p className="text-sm text-gray-400 mt-1">
+                  {agenda.tanggal_event_mulai} - {agenda.tanggal_event_akhir} <span className="ml-2 bg-gray-100 text-xs text-gray-500 font-semibold py-1 px-2 rounded-lg">{agenda.status}</span>
+                </p>
               </div>
             </div>
           ))
